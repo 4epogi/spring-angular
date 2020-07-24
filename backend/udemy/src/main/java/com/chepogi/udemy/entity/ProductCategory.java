3dd28ddae62688357e285f6b1b84096547fe031a
Alexey Shlenskovoy <4epogi@gmail.com>
2020-07-24 14:42:11 +0300

    add product and product category

---
+++ backend/udemy/src/main/java/com/chepogi/udemy/entity/ProductCategory.java
@@ -0,0 +1,26 @@
+package com.chepogi.udemy.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+}
